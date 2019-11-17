5fff9dfee6c101815238fe307a45b2a1a70a40c5
LordSkin <bartek.kramnik@wp.pl>
2019-11-17 21:35:14 +0100

    dodanie obslugi koniotrolera

---
+++ Assets/PlayerControll.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal")*-1, 0.0f, Input.GetAxis("Vertical")*-1);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
