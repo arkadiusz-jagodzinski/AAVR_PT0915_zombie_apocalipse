bb975e515730ca8d21bf39a66c99be70f02a1fa3
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-11-25 21:09:43 +0100

    bugfix

---
+++ Assets/zombieScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zombieScript : MonoBehaviour
+{
+    private Transform goal;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    void Start()
+    {
+
+        //create references
+        goal = Camera.main.transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal.position;
+        GetComponent<Animation>()["Z_Walk"].wrapMode = WrapMode.Loop;
+        GetComponent<Animation>().Play("Z_Walk");
+    }
+}
