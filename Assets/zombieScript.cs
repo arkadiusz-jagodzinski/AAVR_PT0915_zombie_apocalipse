2df0890bfc67904c20b982f48b45495c090f7bde
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-12-20 16:15:09 +0100

    make zombies run better

--- Assets/zombieScript.cs
+++ Assets/zombieScript.cs
@@ -15,8 +15,8 @@ public class zombieScript : MonoBehaviour
         goal = Camera.main.transform;
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.destination = goal.position;
-        GetComponent<Animation>()["Z_Walk"].wrapMode = WrapMode.Loop;
-        GetComponent<Animation>().Play("Z_Walk");
+        GetComponent<Animation>()["Z_Run_InPlace"].wrapMode = WrapMode.Loop;
+        GetComponent<Animation>().Play("Z_Run_InPlace");
 
     }
 
