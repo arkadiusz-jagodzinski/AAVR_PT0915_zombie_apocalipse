10c3ede9d24b71a4b713bbf2f3872a80c439e7c8
LordSkin <bartek.kramnik@wp.pl>
2019-12-22 22:42:21 +0100

    nowy system respienia zombie - sterowany skryptowo

---
+++ Assets/GameState.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState
+{
+    private static int zombiesCount = 2;
+
+    private static readonly List<Vector3> respPoints = new List<Vector3>
+    {
+        new Vector3(-15,0,2),
+        new Vector3(-15,0,-10),
+        new Vector3(15,0,-10),
+        new Vector3(4,11,-33),
+        new Vector3(10,11,-6),
+        new Vector3(33,5,-17)
+    };
+
+    private static GameState gameState;
+
+    public static GameState getGameState()
+    {
+        if (gameState == null) gameState = new GameState();
+        return gameState;
+    }
+
+    private GameState() { }
+
+    public int getZombiesCount()
+    {
+        return zombiesCount;
+    }
+
+
+    public GameObject respNewZombie()
+    {
+        zombiesCount++;
+        return respNewZombie();
+    }
+
+    public GameObject respZombie()
+    { 
+        GameObject newZombie = Object.Instantiate(GameObject.Find("zombieZero"));
+        newZombie.GetComponent<Transform>().position = getRandomRespPosition();
+        newZombie.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = true;
+        newZombie.GetComponent<AudioSource>().enabled = true;
+        newZombie.GetComponent<zombieScript>().enabled = true;
+        newZombie.GetComponent<Animation>()["Z_Walk_InPlace"].wrapMode = WrapMode.Loop;
+        newZombie.GetComponent<Animation>().Play("Z_Walk_InPlace");
+        newZombie.GetComponent<CapsuleCollider>().enabled = true;
+
+        return newZombie;
+    }
+
+    private Vector3 getRandomRespPosition()
+    {
+        return respPoints[Random.Range(0, respPoints.Count)];
+    }
+}
