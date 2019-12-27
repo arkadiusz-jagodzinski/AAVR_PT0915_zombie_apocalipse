83c0dc17f62d1cae9c6fa9600754feb2a9c1ec20
GrzegorzHomoncik <226397@student.pwr.edu.pl>
2019-12-27 21:49:44 +0100

    atak zombi w odleglosci 3.1 m

--- Assets/zombieScript.cs
+++ Assets/zombieScript.cs
@@ -7,6 +7,8 @@ public class zombieScript : MonoBehaviour
     private UnityEngine.AI.NavMeshAgent agent;
     public AudioClip deathSound;
     public AudioClip respawnSound;
+    private bool isAttack = false;
+    private bool isDeath = false;
 
     void Start()
     {
@@ -23,47 +25,75 @@ public class zombieScript : MonoBehaviour
     private void Update()
     {
         agent.destination = goal.position;
+        
+        if(agent.remainingDistance != 0 && agent.remainingDistance < 3.1)
+        {
+            isAttack = true;
+            this.attack();
+        }
+        else
+        {
+            isAttack = false;
+            if(!isDeath)
+            GetComponent<Animation>().Play("Z_Run_InPlace");
+        }
+        //Debug.Log(agent.remainingDistance);
+        
+    }
+    public void attack()
+    {
+        if(isAttack)
+        GetComponent<Animation>().Play("Z_Attack");
     }
     //for this to work both need colliders, one must have rigid body, and the zombie must have is trigger checked.
     void OnTriggerEnter(Collider col)
     {
-        Debug.Log("traafienie!");
-        AudioSource.PlayClipAtPoint(deathSound, this.transform.position);
-        //first disable the zombie's collider so multiple collisions cannot occur
-        GetComponent<CapsuleCollider>().enabled = false;
-        //destroy the bullet
-        Destroy(col.gameObject);
-        //stop the zombie from moving forward by setting its destination to it's current position
-        agent.destination = gameObject.transform.position;
-        //stop the walking animation and play the falling back animation
-        GetComponent<Animation>().Stop();
-        GetComponent<Animation>().Play("Z_FallingBack");
-        agent.enabled = false;
-        //destroy this zombie in six seconds.
-        //Destroy(gameObject, 6);
-        //instantiate a new zombie
-
-
-        //set the coordinates for a new vector 3
-        float randomX = UnityEngine.Random.Range(-12f, 12f);
-        float constantY = .01f;
-        float randomZ = UnityEngine.Random.Range(-13f, 13f);
-        //set the zombies position equal to these new coordinates
-
-        //if the zombie gets positioned less than or equal to 3 scene units away from the camera we won't be able to shoot it
-        //so keep repositioning the zombie until it is greater than 3 scene units away. 
-        Vector3 position = new Vector3(randomX, constantY, randomZ);
-        while (Vector3.Distance(position, Camera.main.transform.position) <= 3)
+        Debug.Log(col.name);
+        if (col.name == "Player")
         {
-
-            randomX = UnityEngine.Random.Range(-12f, 12f);
-            randomZ = UnityEngine.Random.Range(-13f, 13f);
-
-            position = new Vector3(randomX, constantY, randomZ);
-            Debug.Log("trying to respawn");
+            
         }
-        Debug.Log("actually respawned");
-        StartCoroutine(respawnZombie(position));
+        else
+        {
+            Debug.Log("traafienie!");
+            isDeath = true;
+            AudioSource.PlayClipAtPoint(deathSound, this.transform.position);
+            //first disable the zombie's collider so multiple collisions cannot occur
+            GetComponent<CapsuleCollider>().enabled = false;
+            //destroy the bullet
+            Destroy(col.gameObject);
+            //stop the zombie from moving forward by setting its destination to it's current position
+            agent.destination = gameObject.transform.position;
+            //stop the walking animation and play the falling back animation
+            GetComponent<Animation>().Stop();
+            GetComponent<Animation>().Play("Z_FallingBack");
+            agent.enabled = false;
+            //destroy this zombie in six seconds.
+           // Destroy(gameObject, 6);
+            //instantiate a new zombie
+           // GameObject zombie = Instantiate(Resources.Load("zombie", typeof(GameObject))) as GameObject;
+
+            //set the coordinates for a new vector 3
+            float randomX = UnityEngine.Random.Range(-12f, 12f);
+            float constantY = .01f;
+            float randomZ = UnityEngine.Random.Range(-13f, 13f);
+            //set the zombies position equal to these new coordinates
+
+            //if the zombie gets positioned less than or equal to 3 scene units away from the camera we won't be able to shoot it
+            //so keep repositioning the zombie until it is greater than 3 scene units away. 
+            Vector3 position = new Vector3(randomX, constantY, randomZ);
+            while (Vector3.Distance(position, Camera.main.transform.position) <= 3)
+            {
+
+                randomX = UnityEngine.Random.Range(-12f, 12f);
+                randomZ = UnityEngine.Random.Range(-13f, 13f);
+
+                position = new Vector3(randomX, constantY, randomZ);
+                Debug.Log("trying to respawn");
+            }
+            Debug.Log("actually respawned");
+            StartCoroutine(respawnZombie(position));
+        }
 
     }
 
@@ -72,10 +102,11 @@ public class zombieScript : MonoBehaviour
     {
         yield return new WaitForSeconds(3f);
         gameObject.transform.position = position;
-        GetComponent<Animation>()["Z_Walk_InPlace"].wrapMode = WrapMode.Loop;
-        GetComponent<Animation>().Play("Z_Walk_InPlace");
+        GetComponent<Animation>()["Z_Run_InPlace"].wrapMode = WrapMode.Loop;
+        GetComponent<Animation>().Play("Z_Run_InPlace");
         GetComponent<CapsuleCollider>().enabled = true;
         AudioSource.PlayClipAtPoint(respawnSound, this.transform.position);
+        isDeath = false;
         agent.enabled = true;
     }
 
